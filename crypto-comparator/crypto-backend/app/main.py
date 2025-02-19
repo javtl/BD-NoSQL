083a189588233135f15bdccf8d49b43aef5d3a39
Carlos Sanchez <carlosjosesm96@gmail.com>
2025-02-19 12:52:11 +0100

    Comentario relevante sobre el main respecto a la dirección IP puesta.

--- crypto-comparator/crypto-backend/app/main.py
+++ crypto-comparator/crypto-backend/app/main.py
@@ -1,3 +1,4 @@
+# Configuración del servidor.
 from fastapi import FastAPI
 from app.routes import router
 
@@ -5,6 +6,7 @@ app = FastAPI(title="Crypto Price Comparator")
 
 app.include_router(router)
 
+# La dirección 127.0.0.1 es local, sólo va a poder accederse desde el ordenador donde se ejecute.
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
