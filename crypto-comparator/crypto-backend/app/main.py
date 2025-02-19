19c52be2ab43500b2ed1ed185ef7c02404adef05
javtl <javierlopeztl1@gmail.com>
2025-02-19 12:29:35 +0100

    actualizacion

---
+++ crypto-comparator/crypto-backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes import router
+
+app = FastAPI(title="Crypto Price Comparator")
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
